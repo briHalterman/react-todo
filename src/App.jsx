dfb7d94b82504e468ba1941833900f266e043aa4
Brittany Halterman <halterma@gmail.com>
2024-11-15 16:02:05 -0500

    Move list into new component

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,18 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
   // const [count, setCount] = useState(0)
-  let todoList = [
-    {
-      id: 1,
-      title: 'Complete Assignment',
-    },
-    {
-      id: 2,
-      title: 'Read Supplemental Materials',
-    },
-    {
-      id: 3,
-      title: 'Check Slack',
-    },
-  ];
+
 
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(({ id, title }) => (
-          <li key={id}>{title}</li>
-        ))}
-      </ul>
+
+      <TodoList />
+
     </div>
   );
 }
