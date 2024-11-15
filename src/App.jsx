1cba52fe98f58e6c4dae42c151b236fe0f9b626a
Brittany Halterman <halterma@gmail.com>
2024-11-15 16:22:31 -0500

    Create AddTodoForm component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-
   return (
     <div>
       <h1>Todo List</h1>
 
-      <TodoList />
+      <AddTodoForm />
 
+      <TodoList />
     </div>
   );
 }
