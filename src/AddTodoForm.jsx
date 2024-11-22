616ba7edce7fe4820a6af17990e8b418c9a4cca0
Brittany Halterman <halterma@gmail.com>
2024-11-21 21:24:08 -0500

    Handle form submission

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 function AddTodoForm() {
+  function handleAddTodo(event) {
+    event.preventDefault();
+
+    const todoTitle = event.target.title.value;
+
+    console.log(todoTitle);
+
+    event.target.reset();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input id="todoTitle"></input>
+      <input id="todoTitle" name="title"></input>
       <button type="submit">Add</button>
     </form>
-  )
-};
+  );
+}
 
 export default AddTodoForm;
