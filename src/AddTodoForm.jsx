f049eb559b10eba62d8b14ef2c323b18f104e99e
Brittany Halterman <halterma@gmail.com>
2024-11-21 21:38:45 -0500

    Store new todo in React state

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function AddTodoForm() {
+function AddTodoForm(props) {
   function handleAddTodo(event) {
     event.preventDefault();
 
     const todoTitle = event.target.title.value;
 
     console.log(todoTitle);
+
+    props.onAddTodo(todoTitle);
 
     event.target.reset();
   }
