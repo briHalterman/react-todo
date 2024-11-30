6dbd9b6d8505ceb3925c4cc2e223a369c5ad67a4
Brittany Halterman <halterma@gmail.com>
2024-11-30 18:40:36 -0500

    Destructure props in TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-// let todoList = [
-//   {
-//     id: 1,
-//     title: 'Complete Assignment',
-//   },
-//   {
-//     id: 2,
-//     title: 'Read Supplemental Materials',
-//   },
-//   {
-//     id: 3,
-//     title: 'Check Slack',
-//   },
-// ];
-
-function TodoList(props) {
+function TodoList({ todoList }) {
   return (
     <ul>
-      {props.todoList.map((todo) => (
+      {todoList.map((todo) => (
         <TodoListItem key={todo.id} todo={todo} />
       ))}
     </ul>
