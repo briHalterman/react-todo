2b06d65530dea0f1270ef360177834b9e20e41c3
Brittany Halterman <halterma@gmail.com>
2024-12-04 14:43:58 -0500

    Read data from local storage when app is rendered

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem('savedTodoList'))
+  );
 
   React.useEffect(() => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
