528dc5d23de6389a8fcd823daa24aaf8138b5c24
Brittany Halterman <halterma@gmail.com>
2025-02-08 21:22:44 -0500

    Upgrade Vite from JavaScript to TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import React, { useState, useEffect } from 'react';
 import { useState, useEffect } from 'react';
 import styles from './App.module.css';
 import TodoList from './TodoList/TodoList';
