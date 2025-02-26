a6be7ebb467156c12697ee153998d2823aafb9d9
Brittany Halterman <halterma@gmail.com>
2025-02-26 13:02:15 -0500

    Implement HomePage route

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,17 @@
 // - Uses React Router for navigation
 
 // Imports
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
+import HomePage from './components/HomePage';
 
 function App() {
   // Render the App
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TodoContainer />} />
-        <Route path="/new" element={<h1>New Todo List</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/todos" element={<TodoContainer />} />
+    </Routes>
   );
 }
 
