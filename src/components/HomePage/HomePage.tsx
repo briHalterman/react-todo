c90f2d2921388ce1757e2b64c46b99f384af456b
Brittany Halterman <halterma@gmail.com>
2025-02-26 19:08:45 -0500

    Add paragraph to home page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -4,6 +4,15 @@ function HomePage() {
   return (
     <div className={styles.container}>
       <h1>Todo List</h1>
+      <p className={styles.paragraph}>
+        Stay organized and boost your productivity with this simple
+        and intuitive Todo List application. Easily add, sort, and
+        manage your tasks with a clean and responsive design. Built
+        with React and TypeScript, it showcases modern web development
+        practices while offering a seamless user experience. Start
+        organizing your tasks today and take control of your to-do
+        list!
+      </p>
     </div>
   );
 }
